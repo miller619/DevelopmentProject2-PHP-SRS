010d7c27307f8e8b1a020af169ab9dd396953ce9
Isteak Ahamed <4310713@students.swinburne.edu.my>
2016-10-27 21:17:13 +0800

    Update update_inventory.php

--- update_inventory.php
+++ update_inventory.php
@@ -61,19 +61,6 @@
 				<input type="submit" value="Submit Query" />
 				<input type="reset" value="Reset"/>
 			</p>
-			<?php>
-	                $ItemID = iID;
-			$checkItemID = mysql_query("SELECT iID FROM UpdateInventory WHERE iID = $ItemID);
-			if(!checkItemID){
-			 echo "Item Added";
-			}
-			if{mysql_num_rows($checkItemID)>0){
-			echo "Item Added ALready Exists";
-			$ItemID = mysql_fecth_array($ItemID);
-			print_r($iID);
-			}
-			
-			<?php/>
 		</form>
 		<hr/>
 	</div>
