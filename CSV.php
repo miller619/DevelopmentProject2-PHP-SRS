3848f1eb1136ed8742a81758848a6ebcd3cc9eca
Isteak Ahamed <4310713@students.swinburne.edu.my>
2016-11-03 00:43:57 +0800

    Update CSV.php

--- CSV.php
+++ CSV.php
@@ -28,3 +28,42 @@
     //close the db connection
     mysqli_close($connect_mysqli);
 ?>
+<!--home page displaying the sales-->
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" type="text/css" href="styles.css"/>
+<div class="bg">
+<header>
+       <nav>
+      <ul>
+        <li class=""><a href="Home.php">Inventory</a>
+          <ul>
+            <li><a href="update_inventory.php">Add</a></li>
+           
+          </ul>
+        </li>
+        <li class=""><a href="display_sales.php">Sales</a>
+          <ul>
+            <li><a href="update_sales.php">Add</a></li>
+            
+          </ul>
+        </li>
+      <li class=""><a href="display_customer.php">Customer</a>
+          <ul>
+            <li><a href="update_customer.php">Add</a></li>
+            
+          </ul>
+        </li>
+    <li class="Disclaimer"><a href="disclaimer.html">Disclaimer</a></li>
+    </ul>
+    </nav>
+  </header>
+
+  <body>
+      <h1>CSV report generated</h1>
+      <p>check upload folder for the file</p>
+  </body>
+<footer>
+       <div id="Footer"> Copyright &copy; 2016  All rights reserved. VAT no. IT 0146971728 </div> 
+</footer>
+</html>
